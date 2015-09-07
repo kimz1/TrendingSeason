74261e3272eaa4bb9a5928a07ed28544fa6addc2
Miroslav Tóth <mirototh1@gmail.com>
2015-09-08 00:22:51 +0200

    ev.stopPropagation() does not work, need to get it done...

--- SeasonalPatterns/WebContent/resources/js/script.js
+++ SeasonalPatterns/WebContent/resources/js/script.js
@@ -15,21 +15,37 @@ function init() {
 var login = {
 	
 	showLogin : function() {
+		var loginContainer = document.getElementById("nav_bar:login:login_container");
+		var win = document.getElementById("nav_bar:login:win_login");
+		loginContainer.setAttribute("class", "login_container_shown");
+		win.setAttribute("class", "win_login_shown");
+		
+		loginContainer.addEventListener("click", stopEvent ,false);
+		function stopEvent(ev) {
+			loginContainer.setAttribute("class", "login_container_hidden");
+			win.setAttribute("class", "win_login_hidden");
+			ev.stopPropagation();
+		}
+	}
+	/*showLogin : function() {
 		login.fadeBackground();
+		login.showLoginWindow();
 	},
 
 	fadeBackground : function() {
 		var elem = document.createElement("div");
 		var body = document.querySelector("body");
+		body.insertBefore(elem, document.getElementById("nav_bar"));
+		elem.setAttribute("class", "overlay");
 		elem.addEventListener("click", function() {
 			body.removeChild(this);
+			var win = document.getElementById("nav_bar:login:win_login").setAttribute("class", "win_login_hidden");
 		}, false);
-		body.appendChild(elem);
-		elem.setAttribute("class", "overlay");
 		console.log("HERE");
 	},
 	
 	showLoginWindow : function() {
-		var win = document.createElement("div");
-	}
+		var win = document.getElementById("nav_bar:login:win_login");
+		win.setAttribute("class", "win_login_shown");
+	}*/
 }
