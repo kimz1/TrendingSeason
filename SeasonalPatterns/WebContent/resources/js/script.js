2191dc464212d1af255f3e4c8c0e570fbc2093ca
Miroslav Tóth <mirototh1@gmail.com>
2015-09-08 22:53:06 +0200

    troubles with javascript

--- SeasonalPatterns/WebContent/resources/js/script.js
+++ SeasonalPatterns/WebContent/resources/js/script.js
@@ -1,4 +1,4 @@
-var loginContainer, loginWin;
+var loginContainer, winContainer, win;
 
 if(window.onload) {
 	var tempOnload = window.onload;
@@ -12,44 +12,21 @@ if(window.onload) {
 }
 
 function init() {
-	loginContainer = document.getElementById("nav_bar:login:login_container");
-	loginWin = document.getElementById("nav_bar:login:win_login");
-	loginContainer = document.getElementById("nav_bar:login:login_container");
-	loginWin = document.getElementById("nav_bar:login:win_login");
-	loginContainer.addEventListener("click", login.doClose ,true);
+	loginContainer = document.getElementById("login_container");
+	winContainer = document.getElementById("win_container");
+	win = document.getElementById("win_login");
+	winContainer.addEventListener("click", login.doClose ,true);
 }
 var login = {
 	
 	showLogin : function() {
-		loginContainer.setAttribute("class", "login_container_shown");
-		loginWin.setAttribute("class", "win_login_shown");
+		loginContainer.setAttribute("class", "login_container_visible");
+		winContainer.setAttribute("class", "win_container_visible");
 	},
 		
 	doClose : function(event) {
-			if(!event.stopPropagation()) console.log("stopPropagation() SHOULD WORK / " + event.stopPropagation());
-			if(!event.preventDefault()) console.log("preventDefault() SHOULD WORK / " + event.preventDefault());
 			loginContainer.setAttribute("class", "login_container_hidden");
-			loginWin.setAttribute("class", "win_login_hidden");
+			winContainer.setAttribute("class", "win_container_hidden");
+			event.stopPropagation();
 	}
-	/*showLogin : function() {
-		login.fadeBackground();
-		login.showLoginWindow();
-	},
-
-	fadeBackground : function() {
-		var elem = document.createElement("div");
-		var body = document.querySelector("body");
-		body.insertBefore(elem, document.getElementById("nav_bar"));
-		elem.setAttribute("class", "overlay");
-		elem.addEventListener("click", function() {
-			body.removeChild(this);
-			var win = document.getElementById("nav_bar:login:win_login").setAttribute("class", "win_login_hidden");
-		}, false);
-		console.log("HERE");
-	},
-	
-	showLoginWindow : function() {
-		var win = document.getElementById("nav_bar:login:win_login");
-		win.setAttribute("class", "win_login_shown");
-	}*/
 };
